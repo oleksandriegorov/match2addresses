9946041956f10cccfdca3bcd1f1bde8a6d677412
root <root@4d730a384dc0.(none)>
2018-10-02 19:40:00 +0000

    Still experimenting with automake

--- src/main.c
+++ src/main.c
@@ -1,4 +1,9 @@
 //
 // Created by Oleksandr Iegorov on 10/2/18.
 //
+#include <stdio.h>
+#include <stdlib.h>
+int main(char *argv,int argc) {
+    printf("Hello World with %d of arguments!\n",argc);
 
+}
